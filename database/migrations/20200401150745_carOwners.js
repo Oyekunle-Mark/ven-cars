b8985d754a1b8795b82ec8ef969fc81dc9c527ee
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-01 15:19:00 +0100

    Fleshed out the car migrations

--- database/migrations/20200401150745_carOwners.js
+++ database/migrations/20200401150745_carOwners.js
@@ -1,8 +1,15 @@
+exports.up = knex => knex.schema.createTable('car_owners', table => {
+  table.increments();
+  table.string('first_name').notNullable();
+  table.string('last_name').notNullable();
+  table.string('email').notNullable();
+  table.string('county').notNullable();
+  table.string('car_model').notNullable();
+  table.integer('car_model_year').notNullable();
+  table.string('car_color').notNullable();
+  table.string('gender').notNullable();
+  table.string('job_title').notNullable();
+  table.text('bio').notNullable();
+});
 
-exports.up = function(knex) {
-  
-};
-
-exports.down = function(knex) {
-  
-};
+exports.down = knex => knex.schema.dropTableIfExists('car_owners');
