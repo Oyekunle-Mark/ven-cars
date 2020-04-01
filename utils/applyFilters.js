21ad6330636b2667abd8b8c4bca1e43272f8d411
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-01 19:05:55 +0100

    First pass solution at the filter function

--- utils/applyFilters.js
+++ utils/applyFilters.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 const db = require('../database/dbConfig');
 
 const getCarOwnersRecord = async () => {
@@ -8,4 +9,55 @@ const getCarOwnersRecord = async () => {
 const getFilters = async () => {
   const filters = await db('car_filters');
   return filters;
+};
+
+const filterRecord = (filter, record) => {
+  const { start_year, end_year, gender, countries, colors } = filter;
+  const result = {};
+
+  for (let i = 0; i < record.length; i++) {
+    const {
+      country,
+      car_model_year,
+      car_color,
+      gender: ownerGender,
+    } = record[i];
+
+    if (
+      !(
+        start_year >= Number(car_model_year) &&
+        end_year <= Number(car_model_year)
+      )
+    ) {
+      continue;
+    }
+
+    if (gender) {
+      if (gender.toLowerCase() !== ownerGender.toLowerCase()) {
+        continue;
+      }
+    }
+
+    if (countries) {
+      const lowerCaseCountries = countries
+        .split(',')
+        .map(ctr => ctr.toLowerCase());
+      const countrySet = new Set(lowerCaseCountries);
+
+      if (!countrySet.has(country.toLowerCase())) {
+        continue;
+      }
+    }
+
+    if (colors) {
+      const lowerCaseColors = colors
+        .split(',')
+        .map(color => color.toLowerCase());
+      const colorSet = new Set(lowerCaseColors);
+
+      if (!colorSet.has(car_color.toLowerCase())) {
+        continue;
+      }
+    }
+  }
 };
