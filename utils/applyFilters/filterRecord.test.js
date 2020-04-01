2baf6a9fc07c6df00f8b1e5575c7df44c1b4f227
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-01 20:18:41 +0100

    Created the test suites blocks

---
+++ utils/applyFilters/filterRecord.test.js
@@ -0,0 +1,7 @@
+describe('filterRecord', () => {
+  describe('Empty field means all records', () => {});
+
+  describe('Returns the expected owner objects', () => {});
+
+  describe('Returns empty array if no match is found', () => {});
+});
