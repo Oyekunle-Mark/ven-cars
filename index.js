2f422722399a0bfdb594e0b763e7ff7776cf53eb
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-01 23:59:56 +0100

    Added a catch to the promise chain

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+/* eslint-disable no-console */
 require('dotenv').config();
 const { server, cache } = require('./api/server');
 const { applyFilters } = require('./utils/applyFilters');
 
 const PORT = process.env.PORT || 5000;
 
-Promise.resolve(applyFilters(cache)).then(() => {
-  // eslint-disable-next-line no-console
-  server.listen(PORT, () => console.log(`Server started at port ${PORT}`));
-});
+Promise.resolve(applyFilters(cache))
+  .then(() => {
+    server.listen(PORT, () => console.log(`Server started at port ${PORT}`));
+  })
+  .catch(err => console.error(`Error starting the server: ${err.toString()}`));
