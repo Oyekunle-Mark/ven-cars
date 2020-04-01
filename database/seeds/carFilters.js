3626b97b56e93dc91673159a7b889644073aa60a
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-01 16:48:18 +0100

    The seeds added to the file

--- database/seeds/carFilters.js
+++ database/seeds/carFilters.js
@@ -1,13 +1,102 @@
-
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+exports.seed = knex =>
+  knex('car_filters')
+    .del()
+    .then(() =>
+      knex('car_filters').insert([
+        {
+          start_year: 1990,
+          end_year: 2010,
+          gender: 'male',
+          countries: [
+            'China',
+            'South Africa',
+            'france',
+            'Mexico',
+            'Japan',
+            'Estonia',
+            'Colombia',
+            'China',
+          ],
+          colors: [
+            'Green',
+            'Violet',
+            'Yellow',
+            'Blue',
+            'Teal',
+            'Maroon',
+            'Red',
+            'Aquamarine',
+            'Orange',
+            'Mauv',
+          ],
+        },
+        {
+          start_year: 1990,
+          end_year: 2010,
+          gender: '',
+          countries: [
+            'China',
+            'South Africa',
+            'france',
+            'Mexico',
+            'Japan',
+            'Estonia',
+            'Colombia',
+            'China',
+          ],
+          colors: [
+            'Green',
+            'Violet',
+            'Yellow',
+            'Blue',
+            'Teal',
+            'Maroon',
+            'Red',
+            'Aquamarine',
+            'Orange',
+            'Mauv',
+          ],
+        },
+        {
+          start_year: 1980,
+          end_year: 2002,
+          gender: 'female',
+          countries: [],
+          colors: [
+            'Green',
+            'Violet',
+            'Yellow',
+            'Blue',
+            'Teal',
+            'Maroon',
+            'Red',
+            'Aquamarine',
+            'Orange',
+            'Mauv',
+          ],
+        },
+        {
+          start_year: 1990,
+          end_year: 2000,
+          gender: '',
+          countries: [],
+          colors: [],
+        },
+        {
+          start_year: 1990,
+          end_year: 2009,
+          gender: '',
+          countries: [
+            'China',
+            'South Africa',
+            'france',
+            'Mexico',
+            'Japan',
+            'Estonia',
+            'Colombia',
+            'China',
+          ],
+          colors: [],
+        },
+      ]),
+    );
