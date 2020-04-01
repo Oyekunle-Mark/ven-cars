e102ae73ec7d1bf045aedef727f3ea11cbe6fbc7
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-01 23:48:50 +0100

    Resolves the filters to the cache before starting the server

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
-const server = require('./api/server');
+const { server, cache } = require('./api/server');
+const { applyFilters } = require('./utils/applyFilters');
 
 const PORT = process.env.PORT || 5000;
 
-// eslint-disable-next-line no-console
-server.listen(PORT, () => console.log(`Server started at port ${PORT}`));
+Promise.resolve(applyFilters(cache)).then(() => {
+  // eslint-disable-next-line no-console
+  server.listen(PORT, () => console.log(`Server started at port ${PORT}`));
+});
