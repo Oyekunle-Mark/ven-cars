9c035045f0215d2ce5a0f059da184ff534e50d15
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-01 16:43:06 +0100

    Fleshed out the carFilter migrations

--- database/migrations/20200401163906_carFilters.js
+++ database/migrations/20200401163906_carFilters.js
@@ -1,8 +1,11 @@
+exports.up = knex =>
+  knex.schema.createTable('car_filters', table => {
+    table.increments();
+    table.integer('start_year').notNullable();
+    table.integer('end_year').notNullable();
+    table.string('gender').notNullable();
+    table.json('countries').notNullable();
+    table.json('colors').notNullable();
+  });
 
-exports.up = function(knex) {
-  
-};
-
-exports.down = function(knex) {
-  
-};
+exports.down = knex => knex.schema.dropTableIfExists('car_filters');
